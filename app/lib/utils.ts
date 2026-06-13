4aba3dfb789590b60e0f58d2fc7d3f81870f6405
Sourav P Bijoy <drvijayapalliyil@gmail.com>
2026-06-13 16:55:00 +0530

    feat: add cn utility and shared loading-spinner component

---
+++ app/lib/utils.ts
@@ -0,0 +1,16 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+/** Escape HTML special characters to prevent injection in email/HTML contexts. */
+export function escapeHtml(str: string): string {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
